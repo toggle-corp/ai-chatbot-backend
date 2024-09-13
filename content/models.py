c19cb1b9b3182a11125670f70ff4f72333a78517
sudan45 <sudan1bhandari@gmail.com>
2024-09-13 17:07:57 +0545

    Add common model

--- content/models.py
+++ content/models.py
@@ -3,14 +3,15 @@ import uuid
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from common.models import UserResource
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=50, null=True, blank=True)
 
 
-class Content(models.Model):
-    # TODO need to create UserResource  models for user model
+class Content(UserResource):
     class DocumentType(models.IntegerChoices):
         WORD = 1, _("Word")
         PDF = 2, _("PDF")
@@ -23,13 +24,11 @@ class Content(models.Model):
 
     title = models.CharField(max_length=100)
     document_type = models.IntegerField(choices=DocumentType.choices)
-    document_file = models.FileField(upload_to="documents")
-    extracted_file = models.FileField(upload_to="documents")
+    document_file = models.FileField(upload_to="documents", blank=True)
+    extracted_file = models.FileField(upload_to="documents", blank=True)
     content_id = models.UUIDField(default=uuid.uuid4, editable=False)
-    description = models.TextField()
-    tag = models.ManyToManyField("Tag")
-    created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey("user.User", on_delete=models.PROTECT)
+    description = models.TextField(help_text="Content text")
+    tag = models.ManyToManyField("Tag", blank=True)
     is_deleted = models.BooleanField(default=False)
-    deleted_at = models.DateTimeField(null=True)
-    deleted_by = models.ForeignKey("user.User", null=True, on_delete=models.PROTECT)
+    deleted_at = models.DateTimeField(blank=True)
+    deleted_by = models.ForeignKey("user.User", null=True, blank=True, on_delete=models.PROTECT)
