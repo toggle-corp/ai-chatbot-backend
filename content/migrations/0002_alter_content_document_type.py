99ef592e2675245edc6f060b43d1a7b02e917c12
sudan45 <sudan1bhandari@gmail.com>
2024-09-18 11:19:31 +0545

    Fix precommit issue

--- content/migrations/0002_alter_content_document_type.py
+++ content/migrations/0002_alter_content_document_type.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('content', '0001_initial'),
+        ("content", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='content',
-            name='document_type',
-            field=models.IntegerField(choices=[(1, 'Word'), (2, 'PDF'), (3, 'Text')], default=3),
+            model_name="content",
+            name="document_type",
+            field=models.IntegerField(choices=[(1, "Word"), (2, "PDF"), (3, "Text")], default=3),
         ),
     ]
