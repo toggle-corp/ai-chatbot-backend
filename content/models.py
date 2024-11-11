d2325684d59b008e735ca91674d7c1363db96e2f
thenav56 <ayernavin@gmail.com>
2024-11-11 17:41:13 +0545

    Fix helm dependency issue

--- content/models.py
+++ content/models.py
@@ -50,6 +50,4 @@ class Content(UserResource):
 
         super().save(*args, **kwargs)
         if self.document_status == self.DocumentStatus.TEXT_EXTRACTED:
-            transaction.on_commit(
-                lambda: create_embedding_for_content_task.delay(self.id)
-            )
+            transaction.on_commit(lambda: create_embedding_for_content_task.delay(self.id))
