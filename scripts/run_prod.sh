79248d31530fb00f93eb775b08072a679694cdb7
Navin Ayer <thenav56@users.noreply.github.com>
2024-11-11 22:51:29 +0545

    Fix/storage (#38)
    * Add s3 integrations
    
    - For alpha using minio
    
    * Use template to generate argocd hooks
    
    * Clean-up workflows

--- scripts/run_prod.sh
+++ scripts/run_prod.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -x
 
-python manage.py collectstatic --noinput &
-python manage.py migrate --noinput
-
 gunicorn main.wsgi:application --bind 0.0.0.0:80
 # gunicorn main.asgi:application --bind 0.0.0.0:80
