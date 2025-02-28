917309e7bf54846bb1e626768d16e2f3e473badc
sudip-khanal <sudipkhanal0777@gmail.com>
2025-02-28 09:40:48 +0545

    Update sehema file

--- main/settings.py
+++ main/settings.py
@@ -333,12 +333,11 @@ else:
     STATIC_ROOT = env("DJANGO_STATIC_ROOT")
     MEDIA_ROOT = env("DJANGO_MEDIA_ROOT")
 # Email
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-# EMAIL_BACKEND= env("EMAIL_BACKEND").upper()
-# if env("EMAIL_HOST_USER"):
-#     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-#     EMAIL_HOST = env("EMAIL_HOST")
-#     EMAIL_PORT = env("EMAIL_PORT")
-#     EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-#     EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-#     EMAIL_USE_SSL = env("EMAIL_USE_SSL")
+EMAIL_BACKEND = env("EMAIL_BACKEND").upper()
+if env("EMAIL_HOST_USER"):
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = env("EMAIL_HOST")
+    EMAIL_PORT = env("EMAIL_PORT")
+    EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+    EMAIL_USE_SSL = env("EMAIL_USE_SSL")
