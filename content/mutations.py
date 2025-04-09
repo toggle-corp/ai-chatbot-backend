eddc020339aab8627d96477f1ac14e336dd2a3c9
sudip-khanal <sudipkhanal0777@gmail.com>
2025-04-09 13:42:07 +0545

    Remove partial true from the content title update mutation

--- content/mutations.py
+++ content/mutations.py
@@ -59,7 +59,6 @@ class PrivateMutation:
             instance=info.context.request.user,
             data=process_input_data(data),
             context={"request": info.context.request},
-            partial=True,
         )
         if errors := mutation_is_not_valid(serializer):
             return MutationResponseType(
